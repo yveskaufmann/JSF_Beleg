8de621a2cc5b37422068a58dcbfef9e14604714b
Yves Kaufmann <fxdapokalypse@googlemail.com>
2016-01-26 07:47:44 +0100

    Configure JPA with a basic test source

---
+++ kbe-ue8-jpa/src/main/java/de/htw_berlin/ai_bachelor/kbe/checklistkbe8jpa/persistence/EMF.java
@@ -0,0 +1,33 @@
+package de.htw_berlin.ai_bachelor.kbe.checklistkbe8jpa.persistence;
+
+import de.htw_berlin.ai_bachelor.kbe.checklistkbe8jpa.model.ToDo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class EMF implements ServletContextListener {
+
+	private static EntityManagerFactory emf;
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		emf = Persistence.createEntityManagerFactory("check");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		emf.close();
+	}
+
+	public static EntityManager createEntityManager() {
+		if (emf == null) {
+			throw new IllegalStateException("Context is not initialized yet.");
+		}
+		return emf.createEntityManager();
+	}
+}
